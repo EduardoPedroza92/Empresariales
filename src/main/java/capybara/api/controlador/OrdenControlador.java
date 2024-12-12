88fd401c109887394ef2060282014fdf1bb5abc0
RataGalvan <omar_galvan@outlook.es>
2024-12-11 20:49:03 -0600

    modificacion controller, se modifico la ruta de api/ordenes a /ordenes

--- src/main/java/capybara/api/controlador/OrdenControlador.java
+++ src/main/java/capybara/api/controlador/OrdenControlador.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/ordenes")
+@RequestMapping("/ordenes")
 public class OrdenControlador {
     @Autowired
     private OrdenServicio ordenServicio;
